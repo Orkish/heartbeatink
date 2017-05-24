d787c9533e3a75b6f9e349f5f2e39b111ce957be
Winson <winson.yuan@accenture.com>
2017-05-24 15:58:53 -0400

    some components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AboutComponent } from './components/about/about.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ReservationsComponent } from './components/reservations/reservations.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    AboutComponent,
+    GalleryComponent,
+    ContactComponent,
+    ReservationsComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
